15e936516663e7aaf5f6929125ca6ad19dde8bf6
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2022-02-05 11:21:01 +0100

    123

---
+++ samfundsfag/pages/components/navabr.jsx
@@ -0,0 +1,10 @@
+import style from '../../styles/modules/Navabr.module.scss'
+
+
+export default function Navbar() {
+    return(
+        <nav>
+            <h1 className={style.Navbar}>Test</h1>
+        </nav>
+    );
+}
--- samfundsfag/pages/index.jsx
+++ samfundsfag/pages/index.jsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from './components/navabr'
 import style from '../styles/modules/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={style.Hello}>
+      <Navbar></Navbar>
       Hello World!
     </div>
   )
